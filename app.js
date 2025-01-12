1c8cf4fa4aa36889508a89772777a9d65eba99d9
s23551 <s23551@pjwstk.edu.pl>
2025-01-12 18:27:12 +0100

    Serving static files.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import Express
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'static')));
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express.js Tutorial');
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
